a45293da185e8936f5bf1c2e8d17e1ddb667d2dd
rebekahthereze <rtlaureano@mcm.edu.ph>
2022-01-07 05:51:17 +0800

    added new page and functionality

--- main.js
+++ main.js
@@ -1,55 +1,44 @@
-let age = document.getElementById('age');
-let gender = document.getElementById('gender');
-let height = document.getElementById('height');
-let weight = document.getElementById('weight');
-let activity = document.getElementById('activity');
-
 let calculate = document.getElementById('calculate');
-let calCount = document.getElementById('calCount');
-let calCount2 = document.getElementById('calCount2');
+let calCount1 = document.getElementById('calCount-id');
 var cal = 0;
 
 calculate.onclick = function() {
-    document.getElementById('food-input').style.visibility = "visible";
-    document.getElementById('food-input');
-    if (gender.value == "1") {
-        var BMR = getBMRMale();
-    } else if (gender.value == "2") {
-        var BMR = getBMRFemale();
+    if (document.getElementById('gender').value == "1") {
+        var BMR = (10 * document.getElementById('weight').value) + (6.25 * document.getElementById('height').value) - (5 * document.getElementById('age').value) + 5;
+    } else if (document.getElementById('gender').value == "2") {
+        var BMR = (10 * document.getElementById('weight').value) + (6.25 * document.getElementById('height').value) - (5 * document.getElementById('age').value) - 161;
     } 
 
     if (activity.value == "1") {
         cal = BMR;
-        calCount.innerHTML = cal + "<br /> calories/day";  
-    } else if (activity.value == "2") {
+    } else if (document.getElementById('activity').value == "2") {
         cal = Math.round(BMR) * 1.2;
-        calCount.innerHTML = cal  + "<br /> calories/day";
-    } else if (activity.value == "3") {
+    } else if (document.getElementById('activity').value == "3") {
         cal = Math.round(BMR * 1.375);
-        calCount.innerHTML = cal + "<br /> calories/day";
-    } else if (activity.value == "4") {
+    } else if (document.getElementById('activity').value == "4") {
         cal = Math.round(BMR * 1.55);
-        calCount.innerHTML = cal + "<br /> calories/day";
-    } else if (activity.value == "5") {
+    } else if (document.getElementById('activity').value == "5") {
         cal = Math.round(BMR * 1.725);
-        calCount.innerHTML =  cal + "<br /> calories/day";
-    } else if (activity.value == "6") {
+    } else if (document.getElementById('activity').value == "6") {
         cal = Math.round(BMR * 1.9);
-        calCount.innerHTML = cal + "<br /> calories/day";
     }
-}
-function getBMRMale() {
-    var BMR = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) + 5;
-    return BMR;
+
+    var calCount = cal;
+    sessionStorage.setItem("calCount", calCount);
+    window.location = 'food_input.html';
 }
 
-function getBMRFemale() {
-    var BMR = (10 * weight.value) + (6.25 * height.value) - (5 * age.value) - 161;
-    return BMR;
+
+function getCalCount() {
+    calCount1.innerHTML = calCount + "<br /> calories/day";
 }
 
-document.getElementById('calculate2').onclick = function() {
-    calCount2.style.visibility = "visible";
-    remCal = cal - document.getElementById('foodCal').value;
-    calCount2.innerHTML = remCal + "<br /> remaining calories today";
+function addFood() {
+    calCount -= document.getElementById('foodCal').value;
+    getCalCount();
+    
+    foodList += document.getElementById('foodName').value + " | " + document.getElementById('foodCal').value + " calories <br />";
+    document.getElementById('foodName').value = '';
+    document.getElementById('foodCal').value = '';
+    document.getElementById('food-list').innerHTML = foodList;
 } 
